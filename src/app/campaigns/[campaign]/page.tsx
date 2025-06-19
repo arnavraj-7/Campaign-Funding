a7887a54cd9a41fb8d238bd1d5cf91667f0ef25c
Arnav Raj <arnavrajcodes@gmail.com>
2025-06-19 13:19:43 +0530

    rough individual campaign page

--- src/app/campaigns/[campaign]/page.tsx
+++ src/app/campaigns/[campaign]/page.tsx
@@ -1,12 +1,30 @@
 "use client"
+import { useContractStore } from '@/stores/contractsStore'
+import { ProcessedCampaign } from '@/types'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const page = async() => {
-    const {campaign} = await useParams();
+const page = () => {
+   const {allCampaigns,isfetching}= useContractStore();
+   const [currentCampain,setCurrent]=useState<ProcessedCampaign|null>(null);
+    const {campaign} = useParams();
+    useEffect(()=>{
+        if(allCampaigns===null) return;
+       const isCurrent = allCampaigns.find((camp:ProcessedCampaign)=>camp.id===Number(campaign));
+       if(!isCurrent) return;
+       setCurrent(isCurrent);
+       console.log(isCurrent.donators);
+    
+    },[allCampaigns])
+    if(isfetching) return <h1>Loading...</h1>
   return (
+
     <div>
-        
+        {currentCampain && <h1>{currentCampain.title}</h1>}
+        {currentCampain&&currentCampain.donators.map((donator,index)=>{
+            return <div key={index}>{donator}</div>
+        })}    
+
     </div>
   )
 }
