96ed923f814f95452c2bf27b416939d296ae8ce3
Arnav Raj <arnavrajcodes@gmail.com>
2025-06-21 00:28:48 +0530

    hardhat changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.27",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
-    }
-  }
+      url: "http://127.0.0.1:8545",
+    },
+    amoy: {
+      url: "https://misty-skilled-butterfly.matic-amoy.quiknode.pro/f355585c6b4701296eeb608908d1f9356cdbe61c/",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
